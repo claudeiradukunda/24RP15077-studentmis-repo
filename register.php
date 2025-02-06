bfa46cf3dc572b38765e08ce5a2766333be969fb
classic <claudeiradukunda1998@gmail.com>
2025-02-06 11:53:53 +0200

    24RP15077, the bootstrap is added

--- register.php
+++ register.php
@@ -14,12 +14,12 @@
             <input type="text" id="names" name="names" required>
             
             <label for="email">Email:</label>
-            <input type="email" id="email" name="email" required>
+            <input type="email" id="email" name="email" required><br>
 
             <label for="password">Password:</label>
-            <input type="password" id="password" name="password" required>
+            <input type="password" id="password" name="password" required><br>
 
-            <button type="submit">Register</button>
+            <button type="submit">Register</button><br>
         </form>
         <p><a href="index.php">Back to Home</a></p>
     </div>
